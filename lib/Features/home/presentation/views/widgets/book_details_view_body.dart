5adbc8bce41ff58129b00ca60697427f8859c6e5
Bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-06-23 21:20:15 +0300

    Textofbookdetaild_complated

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featuredlistviewitem.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/rating.dart';
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,9 +14,19 @@ class BookDetailsViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomBookDetailsAppBar(),
+          SizedBox(height: 10),
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.35,
             child: BookImage(),
+          ),
+          SizedBox(height: 30),
+          Text(
+            'The Jungle Book',
+            style: styles.textStyle30.copyWith(fontFamily: KGtSectraFine),
+          ),
+          Opacity(
+            opacity: .8,
+            child: Text('Rudyard Kipling', style: styles.textStyle14),
           ),
         ],
       ),
