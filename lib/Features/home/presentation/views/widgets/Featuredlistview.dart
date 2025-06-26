944e1fe7c19d4805999bad2ded003dfb74928bda
Bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-06-26 16:31:16 +0300

    fetch_featured_cubit_builded

--- lib/Features/home/presentation/views/widgets/Featuredlistview.dart
+++ lib/Features/home/presentation/views/widgets/Featuredlistview.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/Features/home/data/repos/home_repo_imp.dart';
+import 'package:bookly_app/Features/home/presentation/manager/fetch_featured_books/cubit/fetchfeaturedbooks_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featuredlistviewitem.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class Featuredlistview extends StatelessWidget {
@@ -8,22 +12,32 @@ class Featuredlistview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.29,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                context.go(AppRouter.kBookDetailsview);
-              },
-              child: BookImage(),
-            );
-          },
-        ),
-      ),
+    return BlocBuilder<FetchfeaturedbooksCubit, FetchfeaturedbooksState>(
+      builder: (context, state) {
+        if (state is FetchfeaturedbooksSucessLoaded) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.29,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      context.go(AppRouter.kBookDetailsview);
+                    },
+                    child: BookImage(),
+                  );
+                },
+              ),
+            ),
+          );
+        } else if (state is FetchfeaturedbooksFailureLoaded) {
+          return Text(state.errorMessage);
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
   }
 }
